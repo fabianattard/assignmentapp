119f791dc29a498bd90184414f440d3abcb2a4cb
fabianattard <43746478+fabianattard@users.noreply.github.com>
2019-01-16 23:34:42 +0100

    Updated Small parts

--- title/src/pages/profile/profile.ts
+++ title/src/pages/profile/profile.ts
@@ -15,11 +15,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams) 
+    {
+      
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
-  }
+
 
 }
